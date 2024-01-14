9a4b4b6a1844e0e9d8468e8d8edabbe4b7ef942a
DanilovAnastasia <grey-mice@mail.ru>
2024-01-15 01:12:21 +0300

    написали для ютуба программу которая достает информейшн с помощью API

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,8 @@
+import os
+import json
+from googleapiclient.discovery import build
+
+YT_API_KEY = os.getenv('API_KEY')
 
 
 class Channel:
@@ -5,8 +10,15 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
-    def print_info(self) -> None:
+    @staticmethod
+    def printj(dict_to_print: dict) -> None:
+        """Выводит словарь в json-подобном удобном формате с отступами"""
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+
+    def print_info(self):
         """Выводит в консоль информацию о канале."""
-        pass
+        youtube = build('youtube', 'v3', developerKey=YT_API_KEY)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
