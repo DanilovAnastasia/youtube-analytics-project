4f53453df041c9e5969f08fd02d896d7590cf948
DanilovAnastasia <grey-mice@mail.ru>
2024-01-30 06:43:22 +0300

    add all homework

--- src/video.py
+++ src/video.py
@@ -1,5 +1,4 @@
 import os
-import json
 from googleapiclient.discovery import build
 
 YT_API_KEY = os.getenv('API_KEY')
@@ -35,22 +34,8 @@ class Video:
         youtube = build('youtube', 'v3', developerKey=YT_API_KEY)
         return youtube
 
-    # def to_json(self, dict_to_print) -> None:
-    #     """Сохраняет в файл значения атрибутов экземпляра `Video`"""
-    #
-    #     with open(dict_to_print, "w", encoding='utf-8') as write_file:
-    #         json.dump({"id": self.video_id,
-    #                    "title": self.title,
-    #                    "url": self.video_url,
-    #                    "likeCount": self.like_count,
-    #                    "viewCount": self.video_number_views}, write_file, indent=2, ensure_ascii=False,
-    #                   separators=(',', ': '))
-    #         print(dict_to_print)
-
 
 class PLVideo(Video):
     def __init__(self, video_id, playlist_id):
         super().__init__(video_id)
         self.playlist_id = playlist_id
-
-
